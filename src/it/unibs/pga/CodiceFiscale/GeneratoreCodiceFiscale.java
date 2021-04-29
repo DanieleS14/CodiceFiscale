e8d8d57a61b61f66e49cd88f70f111e2d5cda51f
mario.musicco <mario.musicco@gmail.com>
2021-04-29 15:52:48 +0200

    stiamo sistemando l'output, il resto va

--- src/it/unibs/pga/CodiceFiscale/GeneratoreCodiceFiscale.java
+++ src/it/unibs/pga/CodiceFiscale/GeneratoreCodiceFiscale.java
@@ -1,10 +1,6 @@
 package it.unibs.pga.CodiceFiscale;
 
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamConstants;
-import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.XMLStreamException;
-import java.io.FileInputStream;
 
 public class GeneratoreCodiceFiscale {
 
@@ -20,6 +16,7 @@ public class GeneratoreCodiceFiscale {
 
         String id_string= String.valueOf(id);
         String cognome= prendi_cognome(id_string);
+
         String nome = prendi_nome(id_string);
         String data_nasctita = prendiDataNascita(id_string);
         String codice_comune = prendiComune(id_string);
@@ -33,13 +30,10 @@ public class GeneratoreCodiceFiscale {
     public String prendi_cognome(String id) throws XMLStreamException {
 
         Gestione gs= new Gestione();
+        InterazioneXML interagisci = new InterazioneXML();
 
-        String cognome = leggiDatoXML(id, "cognome");
-        //Quindi qui abbiamo una stringa con il cognome preso dal file testo XML
+        String cognome = interagisci.leggiDatoXML(id, "cognome");
         String cognomeCF= "";
-        //Ora per estrapolare le prime tre consonanti mi ciclo sulla stinga e mi prendo le prime tre
-        //qual'ora non ci fossero abbastanza consonati prendo la prima vocale e qual'ora le lettere non bastassero
-        //metto una X
 
         for(int i = 0; i < cognome.length() && cognomeCF.length() < 3; i++){
             if (gs.controlloConsonanti(cognome.charAt(i))){
@@ -47,12 +41,12 @@ public class GeneratoreCodiceFiscale {
             }
         }
 
-        for (int j=0; j< cognome.length(); j++){
+        for (int j = 0; j < cognome.length(); j++){
             if (cognomeCF.length()<3 && gs.controlloVocali(cognome.charAt(j))){
                 cognomeCF+= cognome.charAt(j);
             }
         }
-        int cdf_ln =cognomeCF.length();
+        int cdf_ln = cognomeCF.length();
         for (int k=0; k< 3-cdf_ln; k++){
             cognomeCF+= "X";
         }
@@ -63,17 +57,12 @@ public class GeneratoreCodiceFiscale {
     public String prendi_nome(String id) throws XMLStreamException {
 
         Gestione gs= new Gestione();
+        InterazioneXML interagisci = new InterazioneXML();
 
-        String nome = leggiDatoXML(id, "nome");
-        //Quindi qui abbiamo una stringa con il gnome preso dal file testo XML
+        String nome = interagisci.leggiDatoXML(id, "nome");
         String nomeCF= "";
-        /*
-        ora dobbiamo fare un passaggio in più rispetto al metodo per il cognome
-        perchè a differenza di quello, ilnome se ha 4 o più consonanti
-        bisogna prendere la prima, la terza e la quarta. nel caso invece che il nome non possiede 4 consonanti
-        si procede esattamente come per il cognome
-         */
-        int conta_consonanti = 0; //per sapere se il nome possiede almeno 4 consonati
+
+        int conta_consonanti = 0;
         for (int i = 0; i < nome.length(); i++){
             if (gs.controlloConsonanti(nome.charAt(i))){
                 conta_consonanti++;
@@ -81,7 +70,7 @@ public class GeneratoreCodiceFiscale {
         }
 
         if (conta_consonanti >= 4){
-            int conto = 0; //mi serve per contare a che consonante sono, in modo tale da saltare la seconda
+            int conto = 0;
             for (int k = 0; k < nome.length() && nomeCF.length() < 3; k++){
                 if (gs.controlloConsonanti(nome.charAt(k))){
                     if (conto == 1){
@@ -113,295 +102,73 @@ public class GeneratoreCodiceFiscale {
         return nomeCF;
     }
 
-    /**
-     * DA VEDERE SE SI RIESCE AD OTTIMMIZZARE (io ho una mezza idea)
-     * il metodo per il giorno ho usato uno mio ma è da sostituire con quello di Daniele
-     * Ho momentaneamente utilizzato il mio perchè non capendo troppo il codice scritto mi dava errori
-     * quindi appena ci vediamo me lo spieghi e lo uniamo
-     *
-     * metodo che prende in input la data completa e mi torna il pezzo di codice fiscale corrispondente
-     * per l'anno prende le utime due cifre, per il mese avviene una conversione del numero
-     * alla lettera corrispondente, e il giorno va in base al sesso, se femminile aumenta di 40, altrimenti
-     * resta tale
-     * @param id
-     * @return
-     * @throws XMLStreamException
-     */
+
     public String prendiDataNascita(String id) throws XMLStreamException{
 
         Gestione gs = new Gestione();
+        InterazioneXML interagisci = new InterazioneXML();
 
-        String data = leggiDatoXML(id, "data_nascita");
-        String sesso = leggiDatoXML(id, "sesso");
+        String data = interagisci.leggiDatoXML(id, "data_nascita");
+        String sesso = interagisci.leggiDatoXML(id, "sesso");
         String codice_nascita = "";
         String anno = String.valueOf(data.charAt(2)) + String.valueOf(data.charAt(3));
         String mese = String.valueOf(data.charAt(5)) + String.valueOf(data.charAt(6));
         Integer ms = Integer.valueOf(mese);
         char codice_mese = gs.conversioneMese(ms);
-        String giorno_nascita = String.valueOf(data.charAt(8)) + String.valueOf(data.charAt(9));
-        //String data_giorno = giorno(sesso, giorno_nascita);
-        Integer gg = Integer.valueOf(giorno_nascita);
         String giorno = giorno(sesso, data);
         codice_nascita = anno + codice_mese + giorno;
 
         return codice_nascita;
     }
 
-    /**
-     *metodo che prende sia il sesso della persona sia la sua data di nascita, attraverso il metodo split dividiamo la data rispettivamente
-     * parti[0] contiene l'anno, parti[1] il mese, parti[2] contiene il giorno che prenderemo trasformandolo in un intero per
-     * poter aggiungere +40 in caso si stia parlando di una femmina o nel caso di un maschio non aggiungere nulla.
-     * fatto ciò viene ritrasformato in una stringa per poterlo restituire.
-     * @param sesso
-     * @param data
-     * @return
-     */
+
     public String giorno(String sesso, String data){
+
         String[] parti = data.split("-");
         String data_giorno = parti[2];
-    //       String DataGiorno = parti[2];
         int risultato_intero = Integer.parseInt(data_giorno);
-        if(sesso.equals("F"))
-            risultato_intero = risultato_intero + 40;
 
+        if(sesso.equals("F")) {
+            risultato_intero = risultato_intero + 40;
+        }
         String risultato = String.valueOf(risultato_intero);
+
         if (risultato.length()== 1){
             risultato= "0"+ risultato;
         }
         return risultato;
     }
 
-    /**
-     * Metodo per i comuni di nascita:
-     *     da input si prende il comune che entra  in un controllo nella lista dei comuni xml
-     *     e ci ritorna il codice del comune
-     *     poi ritornerà la solita stringa che andrà alla fine ad unirsi al cf
-     * @param id
-     * @return
-     * @throws XMLStreamException
-     */
+
     public String prendiComune(String id) throws XMLStreamException{
-        String comune_di_nascita = leggiDatoXML(id, "comune_nascita");
-        //metodo di ricerca inserendo una stringa mi deve tornare ciò che mi serve
-        //sarà comune perchè mi serve in più parti
-        String codice_comune = leggiComuneXML(comune_di_nascita);
+
+        InterazioneXML interagisci = new InterazioneXML();
+
+        String comune_di_nascita = interagisci.leggiDatoXML(id, "comune_nascita");
+        String codice_comune = interagisci.leggiComuneXML(comune_di_nascita);
+
         return codice_comune;
     }
 
-    /**
-     * secondo me si può generalizzare per adattarelo anche ai controlli che dovremo fare poi
-     * @param stringa_da_cercare
-     * @return
-     */
-//    public String ricerca(String stringa_da_cercare){
-//        boolean trovato = false;
-//        do{
-//            String comune = leggiDatoXML(id)
-//            if(stringa_da_cercare.equals())
-//        }while(!trovato /*|| o sono finiti i comuni ma non penso sia da contemplare*/ );
-//    }
 
-
-   // public String comuni(){
-    //    String codice_comune=imput;
-        // SE risulta dentro lo START_DOCUMENT codice bisogna aquisirlo come stringa
-    //}
-
-
-    /**
-     *  QUESTO BELLISSIMO METODO DI M mi ritorna  LA STRINGA FINALE DEL CF
-     *  se di commenta l'ultima parte e dsi decommenta la parte commentata ritorna la lettera di controllo e basta
-     * @param codice_fiscale
-     * @return
-     */
     public String letteraDiControllo (String codice_fiscale){
+
         Gestione gs = new Gestione();
         int valore = 0;
+
         for(int i = 0; i < 15; i = i + 2){
             valore += gs.tabellaDispari(codice_fiscale.charAt(i));
         }
+
         for(int i = 1; i < 15; i = i + 2){
             valore += gs.tabellaPari(codice_fiscale.charAt(i));
         }
+
         valore = valore % 26;
         char carattere_controllo = gs.tabellaConversione(valore);
         String caratterino_finale = String.valueOf(carattere_controllo);
+
         return caratterino_finale;
-//        codice_fiscale += carattere_controllo;
-//        return codice_fiscale;
     }
-
-    /**
-     * NON TOCCARE
-     *
-     * <br>-Alfiere
-     *
-     * <br> sistemare il ciclo for, implementare un while?
-     *
-     * @param id
-     * @param elemento_necessario
-     * @return carattere_necessario ossia quello che cerchiamo di una persona(nome, cognome, sesso...)
-     * @throws XMLStreamException
-     */
-   public String leggiDatoXML(String id, String elemento_necessario) throws XMLStreamException{
-
-       String carattere_necessario= null;
-       boolean trovato = false;
-
-       XMLInputFactory xmlif = null;
-       XMLStreamReader xmlr = null;
-       try {
-           xmlif = XMLInputFactory.newInstance();
-           xmlr = xmlif.createXMLStreamReader("inputPersone.xml", new FileInputStream("inputPersone.xml"));
-       } catch (Exception e) {
-           System.out.println("Errore nell'inizializzazione del reader:");
-           System.out.println(e.getMessage());
-       }
-       while (xmlr.hasNext() && !trovato){
-
-          if (xmlr.getEventType()==XMLStreamConstants.START_ELEMENT && xmlr.getAttributeCount()>0 && xmlr.getAttributeValue(0).equals(id)) {//START_ELEMENT dà come risultato un intero
-              xmlr.next();
-              while (!controlloInput(xmlr)) {
-                  xmlr.next();
-
-                  if (xmlr.getEventType()==XMLStreamConstants.START_ELEMENT && xmlr.getLocalName().equals(elemento_necessario)){
-                      xmlr.next();
-                      carattere_necessario= xmlr.getText();
-                      //è possibile andare a cancellare un elemento dal XML dato?
-                      trovato= true;
-                      break;
-                  }
-
-              }
-          }
-          xmlr.next();
-       }
-       return carattere_necessario;
-   }
-
-    public boolean controlloInput (XMLStreamReader xmlr){
-        if (xmlr.getEventType()== XMLStreamConstants.END_ELEMENT){
-            if (xmlr.getLocalName().equals("persona")){
-                return true;
-            }
-        }
-        return false;
-    }
-
-
-    /**
-     * per funzionare funziona. è da capire e sistemare se si vuole
-     *
-     */
-    public String leggiComuneXML(String stringa_da_trovare) throws XMLStreamException{
-
-        //String carattere_necessario= null;
-        boolean trovato = false;
-
-        XMLInputFactory xmlif = null;
-        XMLStreamReader xmlr = null;
-        try {
-            xmlif = XMLInputFactory.newInstance();
-            xmlr = xmlif.createXMLStreamReader("comuni.xml", new FileInputStream("comuni.xml"));
-        } catch (Exception e) {
-            System.out.println("Errore nell'inizializzazione del reader:");
-            System.out.println(e.getMessage());
-        }
-
-        String codice_comune = "";
-        while (xmlr.hasNext() && !trovato){
-            if (xmlr.getEventType()== XMLStreamConstants.START_ELEMENT && xmlr.getLocalName().equals("comune")) {
-                while (!controlloComuni(xmlr)){
-                    if (xmlr.getEventType() == XMLStreamConstants.START_ELEMENT && xmlr.getLocalName().equals("nome")) {
-                        xmlr.next();
-                        if (xmlr.getEventType()==XMLStreamConstants.CHARACTERS && xmlr.getText().equals(stringa_da_trovare)){
-
-                            do{
-                                xmlr.next();
-                            }while(!controlloCodiceComune(xmlr));
-
-                            codice_comune += xmlr.getText();
-                            trovato = true;
-                            break;
-                        }
-                    }
-                    xmlr.next();
-                }
-            }
-            xmlr.next();
-        }
-        return codice_comune;
-
-    }
-
-    public boolean controlloComuni (XMLStreamReader xmlr){
-        if (xmlr.getEventType()== XMLStreamConstants.END_ELEMENT){
-            if (xmlr.getLocalName().equals("comune")){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean controlloCodiceComune(XMLStreamReader xmlr){
-        if (xmlr.getEventType()==XMLStreamConstants.CHARACTERS && xmlr.getText().trim().length() > 0) {
-            return true;
-        }
-        return false;
-    }
-
-
-    /*
-    DA FINIRE COGLIONE MARIO
-
-    si può fare anche come boolean ma dipende da dove lo mettiamo
-     */
-    public boolean controllaCodiciFiscaliXML(String stringa_da_trovare) throws XMLStreamException{
-        boolean spaiato = true;
-        String esistenza = null;
-        boolean trovato = false;
-
-        XMLInputFactory xmlif = null;
-        XMLStreamReader xmlr = null;
-        try {
-            xmlif = XMLInputFactory.newInstance();
-            xmlr = xmlif.createXMLStreamReader("codiciFiscali.xml", new FileInputStream("codiciFiscali.xml"));
-        } catch (Exception e) {
-            System.out.println("Errore nell'inizializzazione del reader:");
-            System.out.println(e.getMessage());
-        }
-
-        while (xmlr.hasNext() && !trovato){
-            if (xmlr.getEventType()==XMLStreamConstants.CHARACTERS && xmlr.getText().equals(stringa_da_trovare)) {
-//                while (!controlloSuCodici(xmlr)){
-//                    if(xmlr.getEventType()==XMLStreamConstants.CHARACTERS && xmlr.getText().equals(stringa_da_trovare)) {
-////                        do{
-////                            xmlr.next();
-////                        }while(!controlloCodiceComune(xmlr));
-//
-//                        esistenza = xmlr.getText();
-//                        trovato = true;
-//
-//                        break;
-//                    }
-//                    xmlr.next();
-//                }
-                esistenza = xmlr.getText();
-                spaiato = false;
-                trovato = true;
-
-                break;
-            }
-            xmlr.next();
-        }
-        if(trovato == false) {
-            System.out.println("ASSENTE");
-
-        }else{
-            System.out.println(esistenza);
-        }
-        return spaiato;
-    }
-
 
 }
--- src/it/unibs/pga/CodiceFiscale/InterazioneXML.java
+++ src/it/unibs/pga/CodiceFiscale/InterazioneXML.java
@@ -3,47 +3,62 @@ package it.unibs.pga.CodiceFiscale;
 import javax.xml.stream.*;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamException;
 
 public class InterazioneXML {
 
 
-//    public void ScritturaXML() {
-//        XMLOutputFactory xmlof = null;
-//        XMLStreamWriter xmlw = null;
-//        try {
-//            xmlof = XMLOutputFactory.newInstance();
-//            //non riesco a capire come risolvere il problmea nella riga 16 e 17 nel loro pdf dice che dovrebbe
-//            // stampare questo <?xml version="1.0" encoding="UTF-8"?>
-//            xmlw = xmlof.createXMLStreamWriter(new FileOutputStream("codiciPersone.xml"), “utf - 8”);
-//            xmlw.writeStartDocument(“utf - 8”, “ 1.0”);
-//        } catch (Exception e) {
-//            System.out.println("Errore nell'inizializzazione del writer:");
-//            System.out.println(e.getMessage());
-//        }
-//
-//        //qui bisogna inserire la stringa da fargli scrivere
-//        String[] autori = {"Roberto", "Jacopo", "Enrico", "Stefano"}; // esempio di dati da scrivere
-//
-//        try { // blocco try per raccogliere eccezioni
-//
-//            xmlw.writeStartElement("programmaArnaldo"); // scrittura del tag radice <programmaArnaldo>
-//            xmlw.writeComment("INIZIO LISTA"); // scrittura di un commento
-//            for (int i = 0; i < autori.length; i++) {
-//                xmlw.writeStartElement("autore"); // scrittura del tag autore...
-//                xmlw.writeAttribute("id", Integer.toString(i)); // ...con attributo id...
-//                xmlw.writeCharacters(autori[i]); // ...e content dato
-//                xmlw.writeEndElement(); // chiusura di </autore>
-//            }
-//            xmlw.writeEndElement(); // chiusura di </programmaArnaldo>
-//            xmlw.writeEndDocument(); // scrittura della fine del documento
-//            xmlw.flush(); // svuota il buffer e procede alla scrittura
-//            xmlw.close(); // chiusura del documento e delle risorse impiegate
-//        } catch (Exception e) { // se c’è un errore viene eseguita questa parte
-//            System.out.println("Errore nella scrittura");
-//        }
-//
-//
-//    }
+    public void ScritturaXML(int num_pers) {
+
+        XMLOutputFactory xmlof = null;
+        XMLStreamWriter xmlw = null;
+        try {
+            xmlof = XMLOutputFactory.newInstance();
+            xmlw = xmlof.createXMLStreamWriter(new FileOutputStream("outputPersone.xml"), "utf-8");
+            xmlw.writeStartDocument("utf-8", "1.0");
+        } catch (Exception e) {
+            System.out.println("Errore nell'inizializzazione del writer:");
+            System.out.println(e.getMessage());
+        }
+
+
+        try { // blocco try per raccogliere eccezioni
+
+            xmlw.writeStartElement("output");
+
+            xmlw.writeStartElement("persone");
+            xmlw.writeAttribute("numero", Integer.toString(num_pers));
+            for (int i = 0; i< num_pers; i++){
+
+            }
+            xmlw.writeEndElement();
+            xmlw.writeEndDocument();
+
+        } catch (Exception e) { // se c’è un errore viene eseguita questa parte
+            System.out.println("Errore nella scrittura");
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public void leggiCodiceXML() throws XMLStreamException {
 
@@ -76,5 +91,144 @@ public class InterazioneXML {
         }
         System.out.println(corretti);
         //return carattere_necessario;
+    }
+
+
+    public boolean controllaCodiciFiscaliXML(String stringa_da_trovare) throws XMLStreamException{
+        boolean spaiato = true;
+        String esistenza = null;
+        boolean trovato = false;
+
+        XMLInputFactory xmlif = null;
+        XMLStreamReader xmlr = null;
+        try {
+            xmlif = XMLInputFactory.newInstance();
+            xmlr = xmlif.createXMLStreamReader("codiciFiscali.xml", new FileInputStream("codiciFiscali.xml"));
+        } catch (Exception e) {
+            System.out.println("Errore nell'inizializzazione del reader:");
+            System.out.println(e.getMessage());
+        }
+
+        while (xmlr.hasNext() && !trovato){
+            if (xmlr.getEventType()==XMLStreamConstants.CHARACTERS && xmlr.getText().equals(stringa_da_trovare)) {
+                esistenza = xmlr.getText();
+                spaiato = false;
+                trovato = true;
+
+                break;
+            }
+            xmlr.next();
+        }
+        if(trovato == false) {
+            System.out.println("ASSENTE");
+
+        }else{
+            System.out.println(esistenza);
+        }
+        return spaiato;
+    }
+
+
+    public String leggiComuneXML(String stringa_da_trovare) throws XMLStreamException{
+
+        //String carattere_necessario= null;
+        boolean trovato = false;
+
+        XMLInputFactory xmlif = null;
+        XMLStreamReader xmlr = null;
+        try {
+            xmlif = XMLInputFactory.newInstance();
+            xmlr = xmlif.createXMLStreamReader("comuni.xml", new FileInputStream("comuni.xml"));
+        } catch (Exception e) {
+            System.out.println("Errore nell'inizializzazione del reader:");
+            System.out.println(e.getMessage());
+        }
+
+        String codice_comune = "";
+        while (xmlr.hasNext() && !trovato){
+            if (xmlr.getEventType()== XMLStreamConstants.START_ELEMENT && xmlr.getLocalName().equals("comune")) {
+                while (!controlloComuni(xmlr)){
+                    if (xmlr.getEventType() == XMLStreamConstants.START_ELEMENT && xmlr.getLocalName().equals("nome")) {
+                        xmlr.next();
+                        if (xmlr.getEventType()==XMLStreamConstants.CHARACTERS && xmlr.getText().equals(stringa_da_trovare)){
+
+                            do{
+                                xmlr.next();
+                            }while(!controlloCodiceComune(xmlr));
+
+                            codice_comune += xmlr.getText();
+                            trovato = true;
+                            break;
+                        }
+                    }
+                    xmlr.next();
+                }
+            }
+            xmlr.next();
+        }
+        return codice_comune;
+
+    }
+
+    public boolean controlloComuni (XMLStreamReader xmlr){
+        if (xmlr.getEventType()== XMLStreamConstants.END_ELEMENT){
+            if (xmlr.getLocalName().equals("comune")){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean controlloCodiceComune(XMLStreamReader xmlr){
+        if (xmlr.getEventType()==XMLStreamConstants.CHARACTERS && xmlr.getText().trim().length() > 0) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public String leggiDatoXML(String id, String elemento_necessario) throws XMLStreamException{
+
+        String carattere_necessario= null;
+        boolean trovato = false;
+
+        XMLInputFactory xmlif = null;
+        XMLStreamReader xmlr = null;
+        try {
+            xmlif = XMLInputFactory.newInstance();
+            xmlr = xmlif.createXMLStreamReader("inputPersone.xml", new FileInputStream("inputPersone.xml"));
+        } catch (Exception e) {
+            System.out.println("Errore nell'inizializzazione del reader:");
+            System.out.println(e.getMessage());
+        }
+        while (xmlr.hasNext() && !trovato){
+
+            if (xmlr.getEventType()==XMLStreamConstants.START_ELEMENT && xmlr.getAttributeCount()>0 && xmlr.getAttributeValue(0).equals(id)) {//START_ELEMENT dà come risultato un intero
+                xmlr.next();
+                while (!controlloInput(xmlr)) {
+                    xmlr.next();
+
+                    if (xmlr.getEventType()==XMLStreamConstants.START_ELEMENT && xmlr.getLocalName().equals(elemento_necessario)){
+                        xmlr.next();
+                        carattere_necessario= xmlr.getText();
+                        //è possibile andare a cancellare un elemento dal XML dato?
+                        trovato= true;
+                        break;
+                    }
+
+                }
+            }
+            xmlr.next();
+        }
+        return carattere_necessario;
+    }
+
+    public boolean controlloInput (XMLStreamReader xmlr){
+        if (xmlr.getEventType()== XMLStreamConstants.END_ELEMENT){
+            if (xmlr.getLocalName().equals("persona")){
+                return true;
+            }
+        }
+        return false;
     }
 }
